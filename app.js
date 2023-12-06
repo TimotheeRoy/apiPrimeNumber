fc29d50cf3f111960b187d8f1fce3728a20ee008
TimotheeRoy <timroy93100@gmail.com>
2023-12-06 09:44:03 +0100

    déploiement sur vercel fonctionnel

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+
+const isNumberPrime = (n) =>{
+    if (n < 2)
+        return false
+    for (let i = 2 ; i <= Math.sqrt(n) ; i++){
+        if (n%i === 0)
+            return false
+    }
+    return true
+}
+
+app.get('/isPrime', (req, res) => {
+    const n = parseInt(req.query.n)
+    res.header('Content-Type', 'application/json')
+    res.json({
+        n, 
+        isPrime: isNumberPrime(n) 
+    })
+})
+
+
+export default app;
